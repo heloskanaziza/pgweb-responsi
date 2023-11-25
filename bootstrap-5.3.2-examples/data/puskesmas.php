8e02ab703a4b37c6d60823f64697b4b21e56654f
heloskanaaziza <heloskanaaziza@gmail.com>
2023-11-25 11:30:24 +0700

    update

--- bootstrap-5.3.2-examples/data/puskesmas.php
+++ bootstrap-5.3.2-examples/data/puskesmas.php
@@ -12,7 +12,8 @@
   <title>Pur(e)Health</title>
 
   <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/album/">
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  
 
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
